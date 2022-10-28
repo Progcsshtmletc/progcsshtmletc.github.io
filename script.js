ed18d06a9b8b80e787449957679eb221c3aa1dae
Progcsshtmletc <116519007+Progcsshtmletc@users.noreply.github.com>
2022-10-28 19:51:00 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let phrases = [
-  { text: 'отправить другу смешную гифку', image: 'https://code.s3.yandex.net/web-code/procrastinate/1.gif' },
+  { text: "отправить другу смешную гифку", image: 'https://code.s3.yandex.net/web-code/procrastinate/1.gif' },
   { text: 'посмотреть скидки на авиабилеты', image: 'https://code.s3.yandex.net/web-code/procrastinate/2.png' },
   { text: 'разобраться, о чём поют рэперы', image: 'https://code.s3.yandex.net/web-code/procrastinate/3.png' },
   { text: 'расставить книги на полке по цвету', image: 'https://code.s3.yandex.net/web-code/procrastinate/5.png' },
